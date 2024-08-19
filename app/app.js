591f253229c21a395a16ac492bc34b1f501fc136
Mark Vassell <root@mymacs-mbp.lan>
2024-08-19 01:11:51 -0400

    Created the initial template for the application. Using ember js and bluma

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
+  
 }
 
 loadInitializers(App, config.modulePrefix);
